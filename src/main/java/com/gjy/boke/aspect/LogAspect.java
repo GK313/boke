f22da022f49bd473317089bf89a9e1652b8920c0
GK313 <g13144466909@163.com>
2021-01-02 12:23:38 +0800

    日志、静态资源

---
+++ src/main/java/com/gjy/boke/aspect/LogAspect.java
@@ -0,0 +1,87 @@
+package com.gjy.boke.aspect;
+
+
+import com.sun.org.apache.xpath.internal.objects.XObject;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+/**
+ * 处理日志的切面类
+ */
+@Aspect
+@Component
+public class LogAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 切面，以下配置的是截获controller目录下的所有方法
+     */
+    @Pointcut("execution(* com.gjy.boke.controller.*.*(..))")
+    public void log() {}
+
+    /**
+     *在切面方方法执行前执行该前置方法
+     * @param joinPoint
+     */
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint){
+        //获取HttpServlerRequest
+        ServletRequestAttributes sr= (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = sr.getRequest();
+        String url = String.valueOf(request.getRequestURL());
+        String ip = request.getRemoteAddr();
+        //通过切面对象获取拦截到的是哪一个方法
+        String classMethod = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        //获取请求参数
+        Object[] args = joinPoint.getArgs();
+        ResultLog requestLog = new ResultLog(url, ip, classMethod, args);
+        System.out.println("Request {} "+requestLog);
+
+    }
+
+    /**
+     * 在我们拦截到方法执行到返回前拦截，即捕获我们拦截到的方法的返回内容
+     * 如此，通过该方法我们就可以获取到controller中的方法的返回内容
+     * @param result
+     */
+    @AfterReturning(returning = "result",pointcut = "log()")
+    public void doAfterReturning(Object result){
+        //result为请求的页面名称
+        logger.info("Result {}",result);
+    }
+
+    private class ResultLog{
+        private String url;
+        private String ip;
+        private String classMethod;
+        private Object[] args;
+
+        public ResultLog(String url, String ip, String classMethod, Object[] args) {
+            this.url = url;
+            this.ip = ip;
+            this.classMethod = classMethod;
+            this.args = args;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "url='" + url + '\'' +
+                    ", ip='" + ip + '\'' +
+                    ", classMethod='" + classMethod + '\'' +
+                    ", args=" + Arrays.toString(args) +
+                    '}';
+        }
+    }
+
+}
---
+++ src/main/java/com/gjy/boke/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.gjy.boke.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String test(){
+       return "index";
+    }
+}
